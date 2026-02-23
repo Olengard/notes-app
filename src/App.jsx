df059d36e6c8a70d32a6355fb4f0a27149f5bb34
Tuo Nome <tua@email.com>
2026-02-23 23:33:11 +0100

    fix uuid generation for supabase

--- src/App.jsx
+++ src/App.jsx
@@ -224,7 +224,10 @@ function makeJournalNote(folder) {
 }
 
 function generateId() {
-  return Math.random().toString(36).substr(2, 9);
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
 }
 
 function formatDate(iso) {
